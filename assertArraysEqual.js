e1b611cb0fff86ee6a5db92f11ecbef9cf5d5027
James Brown <jamesbrown5292@gmail.com>
2020-08-19 00:58:48 +0000

    finished and linted arrays equal function

---
+++ assertArraysEqual.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  //if the values are the same, return success message
+  if (actual === expected) {
+    console.log(`😀👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+  //...else return fail statement
+    console.log(`😫👎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let head = function(array) {
+  return array[0];
+};
+
+let tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+//Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+//if the heads and tails of the array are the same, then the arrays are the same, so we can return true.
+let eqArrays = function(arr1, arr2) {
+  let tailOne = tail(arr1).join(', ');
+  let tailTwo = tail(arr2).join(', ');
+  if (head(arr1) === head(arr2) && tailOne === tailTwo) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//Implement assertArraysEqual which takes two arrays
+//console.log an appropriate message to the console.
+
+let assertArraysEqual = function(arr1, arr2) {
+  let arraysEqual = eqArrays(arr1, arr2);
+  if (arraysEqual === true) {
+    console.log(`😀👍 Array comparison passed ${arraysEqual} === true`);
+  } else {
+    console.log(`😫👎 Array comparison failed: ${arraysEqual} === false`);
+  }
+};
+
+assertArraysEqual([12, 23, 34], [12, 23, 34]);
