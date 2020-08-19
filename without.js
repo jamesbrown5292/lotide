e744db25d27d71948e29577d7d0b9e77d2ee1d51
James Brown <jamesbrown5292@gmail.com>
2020-08-19 01:20:22 +0000

    completed without.js and linted

--- without.js
+++ without.js
@@ -42,4 +42,19 @@ let assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([12, 23, 34], [12, 23, 34]);
+
+//Implement without which will return a subset of a given array, removing unwanted elements.
+//This function should take in  source array and a itemsToRemove array
+let without = function(source, itemsToRemove) {
+  //iterate through the ITR array, if item in ITR appears in source, apply a filter to source
+  let newArray = source.filter(word => !itemsToRemove.includes(word));
+  return newArray;
+};
+//return a new array with only those elements from source that are not present in the itemsToRemove array.
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
